6af4819ff953eff65fbfd42e22a246f0b44c6ceb
Alcidival Júnior Lemos <alcidival@live.com>
2025-09-05 11:15:39 -0300

    FUNÇÃO DE EXCLUIR
    Configurando a função de excluir curso

--- app/Http/Controllers/CursoController.php
+++ app/Http/Controllers/CursoController.php
@@ -90,6 +90,17 @@ class CursoController extends Controller
      */
     public function destroy(Curso $curso)
     {
-        //
+        //dd('PASSOU AQUI');
+        $deletou = $curso->delete();
+
+        if ($deletou) {
+            return redirect()
+                ->route('curso.index')
+                ->with('success', 'Curso removido com sucesso!');
+        } else {
+            return redirect()
+                ->route('curso.index')
+                ->with('error', 'Não foi possível remover o curso!');
+        }
     }
 }
